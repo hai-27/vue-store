dd720bbdd6bb7ea3528b9050477cea0fc0dbc10c
chenjiahai1 <chenjiahai1@xiaomi.com>
2021-02-27 04:37:29 +0800

    feat: 删除dist目录

--- dist/js/chunk-6c0851c2.6f13a894.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-6c0851c2"],{"3fb9":function(n,t,e){"use strict";e.r(t);var c=function(){var n=this,t=n.$createElement;n._self._c;return n._m(0)},a=[function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"error"},[e("div",{staticClass:"main"})])}],s=(e("6e21"),e("2877")),r={},i=Object(s["a"])(r,c,a,!1,null,null,null);t["default"]=i.exports},"6e21":function(n,t,e){"use strict";var c=e("ba4c"),a=e.n(c);a.a},ba4c:function(n,t,e){}}]);
-//# sourceMappingURL=chunk-6c0851c2.6f13a894.js.map
