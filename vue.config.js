4bf58f3b25245bffe0aa1d08dedbbba0bae4a966
chenjiahai1 <chenjiahai1@xiaomi.com>
2021-03-03 22:45:47 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
  * @Author: hai-27
  * @Date: 2020-02-07 16:23:00
  * @LastEditors: hai-27
- * @LastEditTime: 2020-03-05 01:41:38
+ * @LastEditTime: 2021-03-03 22:32:57
  */
 module.exports = {
   publicPath: './',
@@ -12,7 +12,7 @@ module.exports = {
     proxy: {
       '/api': {
         // target: 'http://localhost:3000/', // 本地后端地址
-        target: 'http://47.115.85.237:3000/', // 线上后端地址
+        target: 'http://106.15.179.105:3000/', // 线上后端地址
         changeOrigin: true, //允许跨域
         pathRewrite: {
           '^/api': ''
